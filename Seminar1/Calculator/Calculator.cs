fdfeeeed9db14966f5f912fb9f79bd10c604b9b9
Yurgenich <yurakarbushev@gmail.com>
2023-10-20 14:59:20 +0700

    Add Home work 1

---
+++ Seminar1/Calculator/Calculator.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+namespace Seminar1.Calculator
+{
+    public class Calculator
+    {
+        public static int Calculation(int firstOperand, int secondOperand, char @operator)
+        {
+            int result;
+
+            switch (@operator)
+            {
+                case '+':
+                    result = firstOperand + secondOperand;
+                    break;
+                case '-':
+                    result = firstOperand - secondOperand;
+                    break;
+                case '*':
+                    result = firstOperand * secondOperand;
+                    break;
+                case '/':
+                    if (secondOperand != 0)
+                    {
+                        result = firstOperand / secondOperand;
+                        break;
+                    }
+                    else
+                    {
+                        throw new DivideByZeroException("Division by zero is not possible");
+                    }
+                default:
+                    throw new InvalidOperationException("Unexpected value operator: " + @operator);
+            }
+            return result;
+        }
+
+        // HW1.1: Псевдокод для извлечения корня
+        public static double SquareRootExtraction(double num)
+        {
+            if (num < 0)
+            {
+                throw new ArgumentException("Cannot calculate square root of a negative number");
+            }
+            return Math.Sqrt(num);
+        }
+
+        // Метод для вычисления суммы покупки со скидкой
+        public static double CalculatingDiscount(double purchaseAmount, int discountAmount)
+        {
+            if (discountAmount < 0 || discountAmount > 100)
+            {
+                throw new ArgumentException("Discount amount must be between 0 and 100");
+            }
+
+            double discount = (double)discountAmount / 100;
+            double discountedAmount = purchaseAmount * (1 - discount);
+
+            return discountedAmount;
+        }
+    }
+}
---
+++ Seminar1/Shop/Shop.cs
@@ -0,0 +1,47 @@
+﻿using Seminar1.Shop;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Seminar1.Shop
+{
+    public class Shop
+    {
+        private List<Product> products;
+
+        // Геттеры, сеттеры:
+        public List<Product> Products
+        {
+            get { return products; }
+            set { products = value; }
+        }
+
+        // Метод должен вернуть отсортированный по возрастанию по цене список продуктов
+        public List<Product> SortProductsByPrice()
+        {
+            if (products != null)
+            {
+                List<Product> sortedProducts = products.OrderBy(p => p.Cost).ToList();
+                return sortedProducts;
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        // Метод должен вернуть самый дорогой продукт
+        public Product GetMostExpensiveProduct()
+        {
+            if (products != null && products.Count > 0)
+            {
+                Product mostExpensive = products.OrderByDescending(p => p.Cost).First();
+                return mostExpensive;
+            }
+            else
+            {
+                return null;
+            }
+        }
+    }
+}

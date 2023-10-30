46b1f184f13acc9e988120e4bbc18bbe2dbd4597
Yurgenich <yurakarbushev@gmail.com>
2023-10-30 18:07:17 +0700

    Add Homework4 to in Seminar4

---
+++ Seminar4/Book.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTest.Seminar4
+{
+    public class Book
+    {
+        public string Id;
+        public string Title;
+        public string Author;
+
+        public Book(string id)
+        {
+            this.Id = id;
+        }
+
+        public Book(string id, string title, string author) 
+        {
+            this.Id = id;
+            this.Title = title;
+            this.Author = author;
+
+        }
+
+        public string GetId(){ return Id; }
+
+        public void SetId(string id) { this.Id = id; }
+
+        public string GetTitle() { return Title; }
+
+        public void SetTitle(string title) {  this.Title = title; }
+
+        public string GetAuthor() { return Author; }
+
+        public void SetAuthor(string author) {  this.Author = author; }
+    }
+}
---
+++ Seminar4/InMemoryBookRepository.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnitTest.Seminar4;
+
+namespace UnitTest.Seminar4
+{
+    public class InMemoryBookRepository : BookRepository
+    {
+        private readonly Dictionary<string, Book> books;
+
+        public InMemoryBookRepository()
+        {
+            books = new Dictionary<string, Book>();
+            books["1"] = new Book("1", "Book1", "Author1");
+            books["2"] = new Book("2", "Book2", "Author2");
+        }
+
+        public Book FindById(string id)
+        {
+            return books.TryGetValue(id, out var book) ? book : null;
+        }
+
+        public List<Book> FindAll()
+        {
+            return new List<Book>(books.Values);
+        }
+    }
+}
